04153cf2bf27a89993a19f188f802e4e166dea05
A. Eckardt <eckardt.angel7@outlook.com>
2024-10-03 02:35:07 -0400

    [publish] Add information

--- publish.bat
+++ publish.bat
@@ -4,7 +4,7 @@ SET TASK_NAME=Publish to GitHub
 
 git add .
 git commit --message="[publish] Add information"
-git push --upstream origin main
+git push -u origin main
 
 IF %ERRORLEVEL% NEQ 0 (
     ECHO %TASK_NAME% failed with error code %ERRORLEVEL%
